9bbb60e9858207cd0045e09159c1f1a2c7ce100e
wsasouza <wsasouza@hotmail.com>
2021-05-07 19:09:48 -0300

    retrieving token on the server-side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
+import { GetServerSideProps } from "next";
+import { parseCookies } from "nookies";
 import { FormEvent, useContext, useState } from "react";
+
 import { AuthContext } from "../contexts/AuthContext";
 import styles from '../styles/Home.module.css';
 
@@ -35,4 +38,21 @@ export default function Home() {
       <button type="submit">Entrar</button>
     </form>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if (cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
